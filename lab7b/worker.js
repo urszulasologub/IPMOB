15e920063d3b1973d4d9127a40e76329d58385c2
urszulasologub <gold.ula4@gmail.com>
2021-05-10 11:08:30 +0200

    Add lab7b

---
+++ lab7b/worker.js
@@ -0,0 +1,25 @@
+onmessage = function (e) {
+
+  function changeLetterSize(str) {
+    var s = '';
+    var i = 0;
+    while (i < str.length) {
+      var n = str.charAt(i);
+      if (n == n.toUpperCase()) {
+        n = n.toLowerCase();
+      } else {
+        n = n.toUpperCase();
+      }
+      i += 1;
+      s += n; 
+    }
+    return s;
+  }
+
+  var data = JSON.parse(e.data);
+  Object.keys(data).forEach(function (key) { 
+    data[key] = changeLetterSize(data[key]);
+  })
+
+  self.postMessage(JSON.stringify(data));
+};
